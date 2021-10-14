3c516170d5f82d449b235be18f1ea5917550e04f
issiy <allmylifewriter@gmail.com>
2021-10-15 07:41:25 +0900

    【最短最速でフロントエンドの鬼になれ #02 】

---
+++ src/practice/sankou.js
@@ -0,0 +1,21 @@
+const name = 'ishida';
+
+let word = '';
+
+// ifで書いた場合
+// if (name === 'ishida') {
+//   word = '私は石田です。';
+// } else {
+//   word = '私は石田ではありません。';
+// }
+
+// console.log(word);
+
+word =
+  name === 'ishida'
+    ? // 条件式がtrueの場合
+      (word = '私は石田です。')
+    : // 条件式がfalseの場合
+      (word = '私は石田ではありません。');
+
+console.log('sankou', word);
